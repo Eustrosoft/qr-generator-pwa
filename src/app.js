c39d7587bc2451bc2926d47689e1fc48bb5bf1e8
Ilya <riper752@gmail.com>
2024-08-13 22:12:04 +0300

    Added http-server for localhost development Added service-worker.js and cache for offline PWA feature

--- src/app.js
+++ src/app.js
@@ -195,4 +195,14 @@ class QRCodeApp {
 document.addEventListener('DOMContentLoaded', () => {
     const formBuilder = new FormControlBuilder();
     new QRCodeApp(formBuilder);
+
+    if ('serviceWorker' in navigator) {
+        navigator.serviceWorker.register('/service-worker.js')
+            .then(registration => {
+                console.log('ServiceWorker registration successful with scope: ', registration.scope);
+            })
+            .catch(error => {
+                console.log('ServiceWorker registration failed: ', error);
+            });
+    }
 });
